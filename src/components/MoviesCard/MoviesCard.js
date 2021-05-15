37dabd25c820acc6b15938a1822307b2f8ff6ebd
Yulia Nesterova <julianstrv@yandex.ru>
2021-05-16 02:57:37 +0300

    feat: trailer link

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -14,12 +14,14 @@ function MoviesCard(props) {
         year: props.movie.year || 'Не указано',
         description: props.movie.description || 'Не указано',
         image: `${props.movie.image === null ? `${movieImage}` : `https://api.nomoreparties.co${props.movie.image?.url}`}`,
-        trailer: props.movie.trailerLink || 'https://www.youtube.com',
+        trailer: props.movie?.trailerLink,
         nameRU: props.movie.nameRU || 'Не указано',
         nameEN: props.movie.nameEN || 'Не указано',
         thumbnail: `${props.movie.image === null ? `${movieImage}` : `https://api.nomoreparties.co${props.movie.image?.formats?.thumbnail?.url}`}`,
         movieId: props.movie.id,
     }
+
+
 
     const editedDuration = `${Math.trunc(movie.duration/60)}ч${movie.duration % 60}м`;
     const image = `${props.movie.image === null ? `${movieImage}` : `https://api.nomoreparties.co${props.movie.image?.url}`}`;
@@ -62,7 +64,10 @@ function MoviesCard(props) {
 
     return (
         <li className="movies__list-item">
-            <img alt={props.movie.nameRU} src={props.saved ? props.movie.image : image} className="movies__list-poster"/>
+            <a href={props.saved ? props.movie.trailer : props.movie.trailerLink} className="movies__trailer-link">
+                <img alt={props.movie.nameRU} src={props.saved ? props.movie.image : image} className="movies__list-poster"/>
+            </a>
+
             <div onMouseEnter={handleCardMouseOver} onMouseLeave={handleCardMouseOut} className="movies__list-description">
                 <p className="movies__list-title">{props.movie.nameRU}</p>
 
