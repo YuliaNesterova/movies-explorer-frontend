1d3d9165f541761be4069d04287f2723eb102326
Yulia Nesterova <julianstrv@yandex.ru>
2021-05-16 03:32:53 +0300

    feat: logo link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import logo from '../../images/logo.svg';
 import Navigation from "../Navigation/Navigation";
 import './Header.css'
+import {Link} from "react-router-dom";
 
 function Header(props) {
     return (
         <section className={props.main ? 'header' : 'header header_type_white'}>
-            <img className="header__logo" alt="Лого" src={logo}/>
+            <Link to="/"><img className="header__logo" alt="Лого" src={logo}/></Link>
             <Navigation loggedIn={props.loggedIn}/>
         </section>
     )
